009d1901881761db91db531836e382356c0bd22b
Issa <isaiahabdulrahman@gmail.com>
2023-08-31 14:12:46 +0300

    step 1 in 02 done

---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,5 @@
+const imageElement = document.querySelector("#image1");
+const originalImageSrc= imageElement.src;
+imageElement.addEventListener("mouseover", function(){
+    imageElement.src="images/image1_2.jpg";
+})
