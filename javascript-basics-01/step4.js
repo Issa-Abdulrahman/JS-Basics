11cfcac86759d9251cf0131d0e59898f01a45ff9
Issa <isaiahabdulrahman@gmail.com>
2023-08-31 11:17:01 +0300

    taking 3 variables by the user and display them using alert

---
+++ javascript-basics-01/step4.js
@@ -0,0 +1,10 @@
+
+    let  name=prompt(`Enter your name:`)
+    let surname=prompt(`Enter your surname:`)
+    let city= prompt('Enter your city')
+
+ alert(` name : ${name} 
+ surname ${surname} 
+ city: ${city} `);// the $ here is used insted of + and the {} is to specify 
+
+
