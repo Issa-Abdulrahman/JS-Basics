06921106c284f52bccd045cdf5562246ed805d7e
Issa <isaiahabdulrahman@gmail.com>
2023-08-31 15:24:27 +0300

    step 4 in 02 done

---
+++ javascript-basics-02/step 4/step4.js
@@ -0,0 +1,15 @@
+let name = document.querySelector('#name');
+let surname = document.querySelector('#surname');
+let city = document.querySelector('#city');
+let reset = document.querySelector('button');
+
+reset.addEventListener("click",function(){
+let yes = confirm("yes");
+if(yes){
+    name.value="";
+    surname.value="";
+    city.value="";
+}
+ 
+
+})
